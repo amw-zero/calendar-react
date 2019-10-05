759059f18692859ca676cff1ab6f2973e9affa10
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-10-05 18:00:12 -0400

    Antd for styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Form, Input, Button, Card } from 'antd';
 import commands, { makeCalendarShell } from 'calendar-behavior/mod.mjs'
 
 let calendarShell = makeCalendarShell();
-
 function App() {
   const [shell, setShell] = useState(calendarShell);
   const [date, setDate] = useState("");
@@ -16,26 +16,32 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">        
-        <label htmlFor="name-input">Enter a name</label>
-        <input name="name-input" type="test" value={name} onChange={(event) => {
-          setName(event.target.value);
-        }}>
-        </input>
-        <label htmlFor="date-input">Enter a date</label>        
-        <input name="date-input" type="text" value={date} onChange={(event) =>
-          setDate(event.target.value)
-        }>
-        </input>
-        <button onClick={() => {
-          execute(() => commands.addEvent(shell, name, date));
-        }}>
-          Create event
-        </button>
-        { Object.entries(shell.events).map(([date, events]) => {
-          return <span><p>{ date }</p><p>{ name }</p></span>
-        })}
-      </header>
+      <Card size="small">
+        <Form layout="inline">
+          <Form.Item label="Event name">
+            <Input name="name-input" type="test" value={name} onChange={(event) => {
+              setName(event.target.value);
+            }}>
+            </Input>              
+          </Form.Item>
+          <Form.Item label="Event date">
+            <Input name="date-input" type="text" value={date} onChange={(event) =>
+              setDate(event.target.value)
+            }>
+            </Input>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" onClick={() => {
+              execute(() => commands.addEvent(shell, name, date));
+            }}>
+              Create event
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      { Object.entries(shell.events).map(([date, events]) => {
+        return <span><p>{ date }</p><p>{ name }</p></span>
+      })}
     </div>
   );
 }
